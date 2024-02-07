de17ea1a6045bea840779d5e59c8921104a71ae5
jbat100 <johnny.frenchy@gmail.com>
2024-02-07 15:59:22 +0100

    Optional clamp on FloatProcessor, intensity color adaptor

--- packages/com.sonosthesia.utils/Runtime/Processors/FloatProcessor.cs
+++ packages/com.sonosthesia.utils/Runtime/Processors/FloatProcessor.cs
@@ -11,10 +11,28 @@ namespace Sonosthesia.Utils
         [SerializeField] private float _scale = 1f;
 
         [SerializeField] private float _offset;
+
+        [SerializeField] private bool _clamp;
+
+        [SerializeField] private Vector2 _clampRange;
         
         public float Process(float value)
         {
-            return _offset + (_abs ? Mathf.Abs(value) : value) * _scale;
+            float result = value;
+
+            if (_abs)
+            {
+                result = _abs ? Mathf.Abs(value) : value;
+            }
+            
+            result = _offset + result * _scale;
+
+            if (_clamp)
+            {
+                result = Mathf.Clamp(result, _clampRange.x, _clampRange.y);
+            }
+
+            return result;
         }
     }
 }
