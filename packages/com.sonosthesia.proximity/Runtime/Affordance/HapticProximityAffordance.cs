2097e1f9451da15368f4acc519e15f8b27492f65
jbat100 <johnny.frenchy@gmail.com>
2024-03-08 18:21:52 +0100

    Reorganised proximity files and added haptic proximity affordance

---
+++ packages/com.sonosthesia.proximity/Runtime/Affordance/HapticProximityAffordance.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using Sonosthesia.Haptic;
+using UnityEngine;
+
+namespace Sonosthesia.Proximity
+{
+    public class HapticProximityAffordance : ProximityAffordance
+    {
+        [SerializeField] private HapticController _controller;
+        
+        [SerializeField] private Vector3 _offset;
+        
+        [SerializeField] private AnimationCurve _distanceAmplitude;
+        
+        [SerializeField] private AnimationCurve _distanceDuration;
+        
+        [SerializeField] private AnimationCurve _distancePeriod;
+
+        private CancellationTokenSource _rumbleCancellationTokenSource;
+
+        protected virtual void Update()
+        {
+            Vector3 point = transform.TransformPoint(_offset);
+            if (ClosestZone(point, out ProximityZone zone, out Vector3 target, out float distance))
+            {
+                Rumble(distance);
+            }
+            else
+            {
+                StopRumble();
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            StopRumble();
+        }
+
+        private void Rumble(float distance)
+        {
+            if (_rumbleCancellationTokenSource == null)
+            {
+                _rumbleCancellationTokenSource = new CancellationTokenSource();
+                _controller.Rumble(() => _distanceAmplitude.Evaluate(distance),
+                    () => _distanceDuration.Evaluate(distance),
+                    () => _distancePeriod.Evaluate(distance),
+                    _rumbleCancellationTokenSource.Token).Forget();
+            }
+        }
+
+        private void StopRumble()
+        {
+            if (_rumbleCancellationTokenSource != null)
+            {
+                _rumbleCancellationTokenSource.Cancel();
+                _rumbleCancellationTokenSource = null;   
+            }
+        }
+    }
+}
