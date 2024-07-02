b96bd24357d85b527ebfc5d66ccdd996dc7e1f24
jbat100 <johnny.frenchy@gmail.com>
2024-07-02 16:07:51 +0200

    Target framerate setter

---
+++ packages/com.sonosthesia.utils/Runtime/Settings/TargetFramerateSetter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Sonosthesia.Utils
+{
+    // TODO : switch to scriptable object, and switch based on platform
+    
+    public class TargetFramerateSetter : MonoBehaviour
+    {
+        [SerializeField] private int _targetFramerate = -1;
+
+        protected void OnEnable()
+        {
+            Application.targetFrameRate = _targetFramerate;
+        }
+
+    }
+}
