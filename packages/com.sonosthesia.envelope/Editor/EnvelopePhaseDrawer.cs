28bb7993eaf2cd3d916efd36c4a08b21db8782e9
jbat100 <johnny.frenchy@gmail.com>
2024-11-07 09:46:25 +0100

    Aligned envelope phase drawer

--- packages/com.sonosthesia.envelope/Editor/EnvelopePhaseDrawer.cs
+++ packages/com.sonosthesia.envelope/Editor/EnvelopePhaseDrawer.cs
@@ -1,31 +1,24 @@
-using Sonosthesia.Utils;
 using Sonosthesia.Utils.Editor;
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace Sonosthesia.Envelope.Editor
 {
     [CustomPropertyDrawer(typeof(EnvelopePhase))]
-    public class EnvelopePhaseDrawer : PropertyDrawer
+    public class EnvelopePhaseDrawer : HorizontalPropertyDrawer
     {
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        protected override VisualElement CreateContent(SerializedProperty property)
         {
-            VisualElement container = UIHorizontalUtils.CreateContainer(0);
-
-            Label titleLabel = UIHorizontalUtils.CreatePropertyLabel(property.name);
-
+            VisualElement container = UIHorizontalUtils.CreatePropertyContainer();
+            
             SerializedProperty easeTypeProp = property.FindPropertyRelative("_easeType");
             SerializedProperty durationProp = property.FindPropertyRelative("_duration");
 
-            //Label easeTypeLabel = UIHorizontalUtils.CreateLabel("Type", 40);
             EnumField easeTypeField = UIHorizontalUtils.CreateEnumField(easeTypeProp, 125);
 
-            Label durationLabel = UIHorizontalUtils.CreateLabel("sec", 50);
+            Label durationLabel = UIHorizontalUtils.CreateLabel("sec", 25);
             FloatField durationField = UIHorizontalUtils.CreateFloatField(durationProp);
 
-            container.Add(titleLabel);
-            //container.Add(easeTypeLabel);
             container.Add(easeTypeField);
             container.Add(durationField);
             container.Add(durationLabel);
